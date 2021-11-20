af5247957443712e5b9d46949b9f7552d4932d9d
Kris1500 <ciesielski.kris@gmail.com>
2021-11-20 10:10:18 +0100

    proba 1

--- plik testowy.py
+++ plik testowy.py
@@ -1,1 +1,27 @@
-print('test')
+import time
+
+
+def testF(func, arg):
+    start = time.perf_counter()
+    func(arg)
+    stop = time.perf_counter()
+    wynikCzas = stop - start
+    print('czas wykonywania : ', wynikCzas)
+
+def dodawanie(a):
+    for i in range(1000):
+        a=a+1
+    print(a)
+
+def sumujParzyste(a=1,b=10):
+    suma = 0
+    for i in range(b-a+1):
+        if i%2 == 0:
+            suma=suma+i
+    print(suma)
+
+sumujParzyste(1,9)
+
+
+testF(dodawanie,1)
+testF(sumujParzyste,1)
