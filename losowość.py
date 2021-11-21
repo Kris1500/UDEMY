eca7d005c56e7164a25d9ca6a1a4d8d6b64fc48c
Kris1500 <ciesielski.kris@gmail.com>
2021-11-21 07:47:23 +0100

    funkcja wartości broni

--- losowość.py
+++ losowość.py
@@ -1,43 +1,33 @@
 import random
-bazaBroni=[['łuk', 15, 45], ['siekiera', 35, 65], ['miecz', 65, 75], ['nóż', 85, 55]]
-def ciosy(pr):
-    if pr < random.randint(0, 100):
-        print('hit')
-    else:
-        print('not hit')
+import pomocniczy
+bazaBroni=[['łuki', 15, 45], ['siekiera', 35, 65], ['miecz', 65, 75], ['nóż', 85, 55]]
 
 # ilość trafień z broni
 # 1 - łuk, 2- siekiera, 3- miecz, 4 - nóz
 bron = {'łuk': 15, 'siekiera': 35, 'miecz': 65, 'nóż': 85, }
 bron2 = {'łuk': 45, 'siekiera': 65, 'miecz': 75, 'nóż': 55, }
-#bron2 = {'łuk'=[15,35], siekiera = {35,85}, {miecz = 65,65}, nóż = {85,45}}
-#print(bron2)
-listaBroni = {sztuka        #  wyrażenie listowe
-              for sztuka in bron
-              }
-print('Oto nasza dostępna broń: ')
-print(list(bron))
+'''
+listaBroni = [sztuka        #  wyrażenie listowe
+              for sztuka in bazaBroni
+              for sztuka1 in sztuka
+                                          ]
+print(listaBroni) 
+'''
+print('Oto nasza dostępna broń: ', list(bron))
 a=int(input('Podaj skuteczność trafień broni( w % ) która Cię interesuje: '))
 print('Dostępna broń i jej skuteczność trafień')
 for k,v in bron.items():
     if v > a:
         print(k,v)
-print('wybierz broń do analizy')
-bronAnaliza = input('podaj nazwę: ')
+bronAnaliza = input('wybierz broń do analizy, podaj jej nazwę: ')
 if bronAnaliza == 'łuk':
-    print('doskonały wybór!')
-    print('Aby unicestwić rywala w 100 % musisz wykonać')# %s strzałów', %) #popraw %
-    for k,v in bron2.items():
-        if k == bronAnaliza:
-            print(k, 'ma śmiertelnosc ciosów: ', v, '%')
+    pomocniczy.procS(bazaBroni[0][1],bazaBroni[0][2])
 
-bazaStrzałów = []
-i = 0
+elif bronAnaliza == 'siekiera':
+    pomocniczy.procS(bazaBroni[1][1],bazaBroni[1][2])
 
-while i<=3:
-    i+=1
-    ciosy(bazaBroni[0][1])
-bazaStrzałów.append(ciosy(bazaBroni[0][1]))
-from collections import Counter
-print(Counter(bazaStrzałów))
+elif bronAnaliza == 'miecz':
+    pomocniczy.procS(bazaBroni[2][1],bazaBroni[2][2])
 
+elif bronAnaliza == 'nóż':
+    pomocniczy.procS(bazaBroni[3][1],bazaBroni[3][2])
--- pomocniczy.py
+++ pomocniczy.py
@@ -1,27 +1,27 @@
 import random
-#ile strzałów trzeba oddać by osiągnąć 100 %
-def procS(prS):
-    listaS =[]
+#ile strzałów trzeba oddać by osiągnąć 100 % skutecznosci
+def procS(prS, prT=100):
+    listaS =[]  #lista strzałów
     a = prS
     i = 0
+
     while a < 101:
         a+=prS
         i+=1
-    print('dla 100% skuteczności potrzba ', i, 'celnych strzałów')
+    print('dla', prS, '% zadania ran, dla 100% skuteczności wyeliminowania rywala potrzba ', i, 'celnych strzałów')
+    listaS.append(prT)
     listaS.append(i)
-    print(listaS,'lista')
-    print(prS, 'zalozenie procentowe')
+
     j = 0
     l = 0
-    while j < listaS[0]:
+
+    while j < listaS[1]:
         if prS > random.randint(0,100):
                 l+=1
                 j+=1
-                print('j rosnie', j)
+                #print('j rosnie', j)
         else:
                 l+=1
                 j=j
-                print('bez zmian', j)
-    print(l, 'to jest to')
-
-procS(10)
+                #print('bez zmian', j)
+    print('aby to osiągnąć, przy', prT, ' % współczyniku trafień, nalezy oddać', l, 'strzałów')
