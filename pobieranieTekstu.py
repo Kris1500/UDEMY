6bf056effa470953e957fa2ba1808c5f456a72a1
Kris1500 <ciesielski.kris@gmail.com>
2021-11-22 22:10:09 +0100

    funkcja wartości broni

--- pobieranieTekstu.py
+++ pobieranieTekstu.py
@@ -1,8 +1,8 @@
-name = []   # pusta baza
-with open('imieNazwisko.txt', 'r') as file:     # otwiera istniejący plik
+name = []   # empty list
+with open('imieNazwisko.txt', 'r') as file:     # open exist file txt
     for line in file:
-        name.append(tuple(line.replace('\n', '').split(' '))) # tworzy listę krotek
+        name.append(tuple(line.replace('\n', '').split(' '))) # add to list
     print(name)
-with open('imie.txt', 'w') as file:     #tworzy nowy plik tesktowy
+with open('imie.txt', 'w') as file:     # new txt file
     for item in name:
-        file.write(item[0]+'\n')        #dla kazdej krotki w liscie wybiera pierwszy element i zapisuje
+        file.write(item[0]+'\n')        #save new txt file with results
