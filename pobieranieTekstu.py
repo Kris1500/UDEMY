ecc0d7f9f965cb90d6eed1df723ff84583450c94
Kris1500 <94399480+Kris1500@users.noreply.github.com>
2021-11-22 22:05:14 +0100

    zmiana językowa
    Zmiana nazwy 'imię' na 'name'

--- pobieranieTekstu.py
+++ pobieranieTekstu.py
@@ -1,8 +1,8 @@
-imie = []   # pusta baza
+name = []   # pusta baza
 with open('imieNazwisko.txt', 'r') as file:     # otwiera istniejący plik
     for line in file:
-        imie.append(tuple(line.replace('\n', '').split(' '))) # tworzy listę krotek
-    print(imie)
+        name.append(tuple(line.replace('\n', '').split(' '))) # tworzy listę krotek
+    print(name)
 with open('imie.txt', 'w') as file:     #tworzy nowy plik tesktowy
-    for item in imie:
+    for item in name:
         file.write(item[0]+'\n')        #dla kazdej krotki w liscie wybiera pierwszy element i zapisuje
