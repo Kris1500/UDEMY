9077786fc516c03028bf5b6a51301cfd552dc25b
Kris1500 <ciesielski.kris@gmail.com>
2021-12-13 00:46:13 +0100

    kolejne kroki

---
+++ Automat/Automat_Product_Class.py
@@ -0,0 +1,27 @@
+class Product:
+
+    def __init__(self, name, price, pieces):
+        self.name = name
+        self.price = price
+        self.pieces = pieces
+        list_products = []
+    def __str__(self):
+        return (f'Product: {self.name} cost: {self.price} in machine left: {self.pieces} pieces')
+
+#    def count_pieces(self, Product, piec):
+#        self.name = name
+#        self.pieces -= piec
+
+#funkcja zmieniajaca cene
+
+#funkcja zmieniajaca nazwe
+
+#funkcja zmieniajaca ilosc
+
+
+
+Mars = Product('Mars', 3.5, 20)
+WW = Product('WW', 2.5, 20)
+
+print(Mars)
+print(Mars.price)
---
+++ Automat/Automat_Products.py
@@ -0,0 +1,46 @@
+Products = {
+    'Mars': {
+        'name': 'Mars',
+        'price': 3.5,
+        'pieces': 20
+        },
+    'WW': {
+        'name': 'WW',
+        'price': 3.0,
+        'pieces': 20
+        },
+    'Twix': {
+        'name': 'Twix',
+        'price': 4.5,
+        'pieces': 20
+        },
+    'KitKat': {
+        'name': 'KitKat',
+        'price': 3.5,
+        'pieces': 20
+        },
+    'MilkyWay': {
+        'name': 'MilkyWay',
+        'price': 3.0,
+        'pieces': 20
+        }
+}
+
+def count_pieces(name, buy):
+    Products[name]['pieces'] -= buy
+    return Products
+
+def print_one_product(name):
+    return Products[name]
+
+#print(print_one_product('Twix'))
+
+#print(count_pieces('Twix', 5))
+def print_all_products():
+    for product in Products:
+        print(print_one_product(product))
+
+count_pieces('Mars', 3)
+count_pieces('Twix', 2)
+count_pieces('WW', 10)
+print_all_products()
