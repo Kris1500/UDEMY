efba1133b8dd8eeab26616dbbed0cd06169d9416
Kris1500 <ciesielski.kris@gmail.com>
2021-12-20 00:30:55 +0100

    change to english

--- Programy/Wallet/base_currency.py
+++ Programy/Wallet/base_currency.py
@@ -1,47 +1,46 @@
 import datetime
 
-'''
-Zapisuje transakcje kupna kryptowalut do pliku txt.'''
-
 
 class Product:
 
-    def __init__(self, user, name, curs, dolar_curs, amount, value):
+
+    def __init__(self, user, name, curs, dollar_curs, amount, value):
         self.user = user
         self.name = name
         self.curs = curs
-        self.dolar_curs = dolar_curs
+        self.dollar_curs = dollar_curs
         self.amount = amount
         self.value = value
 
     def __str__(self):
-        return (f'{datetime.datetime.now()}\t user {self.user}\t{self.name}\t \t ilosć:\t{self.amount}\t '
-                f'kurs trans:\t \t{self.curs}\tza\t{self.value} \t'
-                f'dolara-kurs:\t{self.dolar_curs}')
-
+        return (f'{datetime.datetime.now()}\t user {self.user}\t{self.name}\t \t amount:\t{self.amount}\t '
+                f'rate:\t \t{self.curs}\tfor\t{self.value} \t'
+                f'$ price:\t{self.dollar_curs}')
 
 def new_t(nr_tr):
-    user = input('Podaj użytkownika: ')
-    name = input('Podaj nazwę kupowanej waluty: ')
-    curs = float(input('Podaj kurs kupna: '))
-    dolar_curs = float(input("Podaj kurs dolara: "))
-    amount = float(input('Podaj ilosc waluty: '))
-    value = float(input('Podaj wartosc: '))
-    nr_tr = Product(user.upper(), name.upper(), curs.__round__(2), dolar_curs.__round__(2), amount.__round__(6),
+    user = input('Username: ')
+    currency = input('Currency: ')
+    buying_rate = float(input('Buying rate: '))
+    dollar_rate = float(input("Dollar rate: "))
+    amount = float(input('Amount: '))
+    value = float(input('Value: '))
+    nr_tr = Product(user.upper(), currency.upper(), buying_rate.__round__(2), dollar_rate.__round__(2),
+                    amount.__round__(6),
                     value.__round__(2))
     buy_list.append(nr_tr)
     return nr_tr
 
-
 def save_txt():
     file = open(r'C:\Users\Milka\PycharmProjects\UDEMY\Programy\Wallet\wallet.txt', 'a')
-    file.write(f'Z A K U P Y')
+    file.write(f'Trade')
     file.write('\n')
     for products in buy_list:
         file.write(f'{products}')
         file.write('\n')
     file.write('\n')
     file.close()
+    print('\n')
+    print(f'Report was saved. \nCreate {len(buy_list)} new position. ')
 
 
 buy_list = []
@@ -51,17 +50,16 @@ def go():
     a = None
     while a != 'Q':
         try:
-            nr_tr = input('Podaj nazwę transakcji: ')
+            nr_tr = input('Name of trasaction: ')
             new_t(nr_tr)
-            print('zapisano!')
-            a = input(" Jeżeli chcesz zakończyć, wybierz 'Q:' i wciśnij enter,\n "
-                      "aby wprowadzić następną pozycję wciśnij dowolny klawisz i enter  ")
+            print('Saved')
+            a = input(" to quit press 'Q:',\n "
+                      "to next trade, press nay key.  ")
         except:
-            print('UPS...  coś poszło nie tak!  Spróbuj ponownie..')
+            print('Error!  Please try again..')
     save_txt()
 
 
 go()
 
-print('\n')
-print(f' Dziękuję! Życzę miłego dnia :) Raport został dopisany do pliku txt. \n Utworzono {len(buy_list)} pozycji. ')
+
