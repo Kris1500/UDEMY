83d9afd871d24dc7e8d4cbfa3f542edad7c16208
Kris1500 <ciesielski.kris@gmail.com>
2022-01-12 23:33:14 +0100

    last step

--- Programy/wregi/frame_program_info.py
+++ Programy/wregi/frame_program_info.py
@@ -1,2 +1,2 @@
-path = "C:\\Users\\Milka\\PycharmProjects\\UDEMY\\Programy\\wregi\\raport.txt"
+# path = "C:\\Users\\Milka\\PycharmProjects\\UDEMY\\Programy\\wregi\\raport.txt"
 path_2 = "Report.txt"
